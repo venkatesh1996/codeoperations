eb4426b1f8b4c1918c905ceab7ab4933a264d686
Venkateshwaran S <54279094+venkatesh1996@users.noreply.github.com>
2026-08-20 16:19:19 +0530

    simple loop formed

--- codeoperations/Program.cs
+++ codeoperations/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
+            int n = 10;
             Console.WriteLine("Starting Program");
+            Console.WriteLine("Starting Loop");
+            for (int i=0; i < n; i++)
+            {
+                Console.WriteLine($"Iteration {i + 1}");
+            })
             Console.WriteLine("Ending Program");
         }
     }
