8d7970176651de2f689a3d1313d6bbccf67fb3f2
Venkateshwaran S <54279094+venkatesh1996@users.noreply.github.com>
2026-08-20 12:08:40 +0530

    initial commit

---
+++ codeoperations/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace codeoperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
