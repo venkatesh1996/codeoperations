10984c93acebfee537511617f40f2e3a5da1adbb
Venkateshwaran S <54279094+venkatesh1996@users.noreply.github.com>
2026-08-20 16:22:29 +0530

    fixed some syntax issues

--- codeoperations/Program.cs
+++ codeoperations/Program.cs
@@ -10,7 +10,7 @@
             for (int i=0; i < n; i++)
             {
                 Console.WriteLine($"Iteration {i + 1}");
-            })
+            }
             Console.WriteLine("Ending Program");
         }
     }
